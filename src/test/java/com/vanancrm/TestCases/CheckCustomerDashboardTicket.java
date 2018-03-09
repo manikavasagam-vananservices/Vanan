183748c7c0a667618bd171c2f9321f8338a51307
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-03-09 18:23:43 +0530

    removed signout in CD

--- src/test/java/com/vanancrm/TestCases/CheckCustomerDashboardTicket.java
+++ src/test/java/com/vanancrm/TestCases/CheckCustomerDashboardTicket.java
@@ -295,7 +295,6 @@ public class CheckCustomerDashboardTicket extends TestBase {
                     false);
         }
         quoteInfo.clickUpdateQuoteInfo();
-        menus.clickSignOut();
     }
 
     private void changePaymentMadeStatus() {
