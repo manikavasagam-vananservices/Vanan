347b02aeaa90c465739645746d560a90503c7362
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-04-18 15:06:51 +0530

    Added fix

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TypingQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TypingQuote.java
@@ -411,8 +411,8 @@ public class TypingQuote extends TestBase implements TypingPrice {
                 if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
-                        .contains(channel) && viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites")) {
+                        .contains(channel) && url.contains(viewTicketDetails
+                        .getRunTimeTicketFieldValues("Websites"))) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
