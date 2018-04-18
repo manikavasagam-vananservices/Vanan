97c159ae5554446b9527be1e17a632e272cc41d5
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-04-18 15:06:27 +0530

    Added fix

--- src/test/java/com/vanancrm/TestCases/DirectPayment/VoiceoverQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/VoiceoverQuote.java
@@ -185,8 +185,8 @@ public class VoiceoverQuote extends TestBase {
                 if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
-                        .contains(channel) && viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites")) {
+                        .contains(channel) && url.contains(viewTicketDetails
+                        .getRunTimeTicketFieldValues("Websites"))) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
