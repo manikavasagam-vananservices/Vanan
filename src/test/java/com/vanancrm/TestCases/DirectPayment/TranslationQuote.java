700a49287de7de27d4442706808e8ed4bdaf02d7
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-04-18 15:07:12 +0530

    Added fix

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TranslationQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TranslationQuote.java
@@ -393,8 +393,8 @@ public class TranslationQuote extends TestBase implements TranslationPrice {
                 if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
-                        .contains(channel) && viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites")) {
+                        .contains(channel) && url.contains(viewTicketDetails
+                        .getRunTimeTicketFieldValues("Websites"))) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
