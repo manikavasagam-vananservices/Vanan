0a3e13f5da667508604809b75e367403308dd4c0
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-07-19 10:59:40 +0530

    Added payment type in WritingServiceQuickQuote

--- src/test/java/com/vanancrm/TestCases/QuickQuote/WritingServiceQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/WritingServiceQuickQuote.java
@@ -198,6 +198,8 @@ public class WritingServiceQuickQuote extends TestBase {
 
         // Edit a ticket and moved the status into Others
         Edit edit = menus.clickEdit();
+        edit.selectPaymentType("Full payment");
+        edit.selectPaymentMode("Square");
         edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
