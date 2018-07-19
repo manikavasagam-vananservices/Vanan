e238b60f655bba012c4759d2caff0c5c0425a986
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-07-19 11:02:02 +0530

    Added payment type in TranscriptionFreeTrialPage

--- src/test/java/com/vanancrm/TestCases/FreeTrial/TranscriptionFreeTrialPage.java
+++ src/test/java/com/vanancrm/TestCases/FreeTrial/TranscriptionFreeTrialPage.java
@@ -213,6 +213,8 @@ public class TranscriptionFreeTrialPage extends TestBase {
 
         // Edit a ticket and moved the status into Others
         Edit edit = menus.clickEdit();
+        edit.selectPaymentType("Full payment");
+        edit.selectPaymentMode("Square");
         edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
