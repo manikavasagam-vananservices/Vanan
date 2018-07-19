33e2a83941d0bd6853849c0695cb59b483cd4b08
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-07-19 10:52:25 +0530

    Added payment type in TranscriptionSentQuote

--- src/test/java/com/vanancrm/TestCases/SentQuote/TranscriptionSentQuote.java
+++ src/test/java/com/vanancrm/TestCases/SentQuote/TranscriptionSentQuote.java
@@ -217,6 +217,8 @@ public class TranscriptionSentQuote extends TestBase {
 
         // Edit a ticket and moved the status into Others
         Edit edit = menus.clickEdit();
+        edit.selectPaymentType("Full payment");
+        edit.selectPaymentMode("Square");
         edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
