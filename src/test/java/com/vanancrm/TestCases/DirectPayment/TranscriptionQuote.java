4bb69e3662d7cd8c1db43f2d8bc47c28c6944f0c
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-07-19 11:11:00 +0530

    Added payment type in WritingService

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TranscriptionQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TranscriptionQuote.java
@@ -561,6 +561,9 @@ public class TranscriptionQuote extends TestBase implements TranscriptionPrice {
 
         // Edit a ticket and moved the status into Others
         Edit edit = menus.clickEdit();
+        
+        edit.selectPaymentType("Full payment");
+        edit.selectPaymentMode("Square");
         edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
