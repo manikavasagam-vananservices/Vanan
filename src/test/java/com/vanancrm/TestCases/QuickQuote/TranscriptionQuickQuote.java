dcc2b647a04cd55a1a3eca2e9c112c43a2e70ee7
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-07-19 10:59:05 +0530

    Added payment type in TranscriptionQuickQuote

--- src/test/java/com/vanancrm/TestCases/QuickQuote/TranscriptionQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/TranscriptionQuickQuote.java
@@ -205,6 +205,8 @@ public class TranscriptionQuickQuote extends TestBase {
 
         // Edit a ticket and moved the status into Others
         Edit edit = menus.clickEdit();
+        edit.selectPaymentType("Full payment");
+        edit.selectPaymentMode("Square");
         edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
