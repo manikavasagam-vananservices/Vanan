70ee17d39cf3a9e7dcfae8176b55a7d97545c793
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-08-17 12:08:21 +0530

    modified email id & website checking

--- src/test/java/com/vanancrm/TestCases/SentQuote/CaptioningSentQuote.java
+++ src/test/java/com/vanancrm/TestCases/SentQuote/CaptioningSentQuote.java
@@ -174,11 +174,11 @@ public class CaptioningSentQuote extends TestBase {
                 waitForProcessCompletion(20);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
-                if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
+                if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
                         .contains(channel) && url.contains(viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites"))) {
+                        .getWebsite())) {
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
                     System.out.println((i + 1) + " : Channel = " +
@@ -277,9 +277,9 @@ public class CaptioningSentQuote extends TestBase {
         System.out.println("===========================================\n");
 
         evaluateCondition("Email", viewTicketDetails
-                .getRunTimeTicketFieldValues("Email"), mailId);
+                .getEmailId(), mailId);
         evaluateCondition("Websites", url,
-                viewTicketDetails.getRunTimeTicketFieldValues("Websites"));
+                viewTicketDetails.getWebsite());
         evaluateCondition("Channel", viewTicketDetails
                 .getRunTimeTicketFieldValues("Channel"), channel);
         evaluateCondition("Video Source Language", viewTicketDetails
