4e2a5d80e17380dbe75ccf2b6550d7ddd89f791d
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-08-17 13:35:47 +0530

    modified email id & website checking

--- src/test/java/com/vanancrm/TestCases/DirectPayment/WritingService.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/WritingService.java
@@ -155,11 +155,11 @@ public class WritingService extends TestBase {
                 waitForProcessCompletion(20);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
-                if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
+                if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
                         .contains(channel) && url.contains(viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites"))) {
+                        .getWebsite())) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
@@ -269,14 +269,14 @@ public class WritingService extends TestBase {
         evaluateCondition("Name", viewTicketDetails
                 .getRunTimeTicketFieldValues("Name"), name);
         evaluateCondition("Email", viewTicketDetails
-            .getRunTimeTicketFieldValues("Email"), mailId);
+            .getEmailId(), mailId);
 
         evaluateCondition("Country", viewTicketDetails
                 .getRunTimeTicketFieldValues("Country"), country);
         evaluateCondition("PhoneNo", viewTicketDetails
                 .getRunTimeTicketFieldValues("PhoneNo"), phoneNumber);
         evaluateCondition("Websites", url,
-            viewTicketDetails.getRunTimeTicketFieldValues("Websites"));
+            viewTicketDetails.getWebsite());
 
         evaluateCondition("Type Of Paper", viewTicketDetails
             .getRunTimeTicketFieldValues("Type Of Paper"), paperType);
