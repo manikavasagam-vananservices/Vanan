d9006f01697d8f39655d4c118210ff8fe22a7fbe
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-08-17 13:34:18 +0530

    modified email id & website checking

--- src/test/java/com/vanancrm/TestCases/DirectPayment/VoiceoverQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/VoiceoverQuote.java
@@ -192,11 +192,11 @@ public class VoiceoverQuote extends TestBase {
                 waitForProcessCompletion(20);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
-                if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
+                if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
                         .contains(channel) && url.contains(viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites"))) {
+                        .getWebsite())) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
@@ -305,14 +305,14 @@ public class VoiceoverQuote extends TestBase {
         evaluateCondition("Name", viewTicketDetails
                 .getRunTimeTicketFieldValues("Name"), name);
         evaluateCondition("Email", viewTicketDetails
-                .getRunTimeTicketFieldValues("Email"), mailId);
+               .getEmailId(), mailId);
 
         evaluateCondition("Country", viewTicketDetails
                 .getRunTimeTicketFieldValues("Country"), country);
         evaluateCondition("PhoneNo", viewTicketDetails
                 .getRunTimeTicketFieldValues("PhoneNo"), phoneNumber);
         evaluateCondition("Websites", url,
-                viewTicketDetails.getRunTimeTicketFieldValues("Websites"));
+                viewTicketDetails.getWebsite());
 
         evaluateCondition("Language", viewTicketDetails
                 .getRunTimeTicketFieldValues("Language"), srcLang);
