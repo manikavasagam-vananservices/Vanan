f5f8d307a9bf7f77e5071e586f9ac462aa3b9c2b
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-08-17 12:51:22 +0530

    modified email id & website checking

--- src/test/java/com/vanancrm/TestCases/QuickQuote/WritingServiceQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/WritingServiceQuickQuote.java
@@ -144,11 +144,11 @@ public class WritingServiceQuickQuote extends TestBase {
                 waitForProcessCompletion(20);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
-                if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
+                if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
                         .contains(channel) && url.contains(viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites"))) {
+                        .getWebsite())) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
@@ -235,7 +235,7 @@ public class WritingServiceQuickQuote extends TestBase {
         System.out.println("===========================================\n");
 
         evaluateCondition("Email", viewTicketDetails
-            .getRunTimeTicketFieldValues("Email"), mailId);
+            .getEmailId(), mailId);
         evaluateCondition("Websites", url,
             viewTicketDetails.getRunTimeTicketFieldValues("Websites"));
         evaluateCondition("Channel", viewTicketDetails
