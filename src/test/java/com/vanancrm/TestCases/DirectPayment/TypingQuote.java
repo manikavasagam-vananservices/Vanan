c3f98298a63c9e5d88cb6f415525b82d79c31df7
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-08-17 13:33:17 +0530

    modified email id & website checking

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TypingQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TypingQuote.java
@@ -433,11 +433,11 @@ public class TypingQuote extends TestBase implements TypingPrice {
                 waitForProcessCompletion(20);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
-                if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
+                if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
                         .contains(channel) && url.contains(viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites"))) {
+                        .getWebsite())) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
@@ -679,9 +679,9 @@ public class TypingQuote extends TestBase implements TypingPrice {
         System.out.println("Checking View Ticket Details");
         System.out.println("===========================================\n");
         evaluateCondition("Email", viewTicketDetails
-                .getRunTimeTicketFieldValues("Email"), mailId);
+                .getEmailId(), mailId);
         evaluateCondition("Websites", url,
-                viewTicketDetails.getRunTimeTicketFieldValues("Websites"));
+                viewTicketDetails.getWebsite());
         evaluateCondition("Channel", viewTicketDetails
                 .getRunTimeTicketFieldValues("Channel"), channel);
         evaluateCondition("Service", viewTicketDetails
