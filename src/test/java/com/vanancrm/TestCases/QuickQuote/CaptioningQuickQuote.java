223cfbe9b10f38a1b1399937a266662ccf7522b2
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-08-17 12:54:09 +0530

    modified email id & website checking

--- src/test/java/com/vanancrm/TestCases/QuickQuote/CaptioningQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/CaptioningQuickQuote.java
@@ -147,11 +147,11 @@ public class CaptioningQuickQuote extends TestBase {
                 waitForProcessCompletion(20);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
-                if (viewTicketDetails.getRunTimeTicketFieldValues("Email")
+                if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel")
                         .contains(channel) && url.contains(viewTicketDetails
-                        .getRunTimeTicketFieldValues("Websites"))) {
+                        .getWebsite())) {
 
                     ticketID = tickets.get(i).substring(tickets.get(i).indexOf("VS"),
                             tickets.get(i).indexOf(service) - 1);
@@ -245,9 +245,9 @@ public class CaptioningQuickQuote extends TestBase {
         System.out.println("===========================================\n");
 
         evaluateCondition("Email", viewTicketDetails
-            .getRunTimeTicketFieldValues("Email"), mailId);
+            .getEmailId(), mailId);
         evaluateCondition("Websites", url,
-            viewTicketDetails.getRunTimeTicketFieldValues("Websites"));
+            viewTicketDetails.getWebsite());
         evaluateCondition("Channel", viewTicketDetails
             .getRunTimeTicketFieldValues("Channel"), channel);
 
