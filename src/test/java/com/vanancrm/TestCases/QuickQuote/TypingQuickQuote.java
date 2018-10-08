bd97516b042d1953fd43e24a9a48238dde9d35f5
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-10-08 12:35:35 +0530

    email id modified

--- src/test/java/com/vanancrm/TestCases/QuickQuote/TypingQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/TypingQuickQuote.java
@@ -41,7 +41,7 @@ public class TypingQuickQuote extends TestBase {
     private String[] languages = {"English", "Apache"};
     private String[] categorys = {"General", "Legal"};
     private String[] fileTypes = {"Document", "Audio/Video"};
-    private String mailId = "automation.vananservices@gmail.com";
+    private String mailId = "automation@vananservices.com";
     private String service = "Typing";
     private String channel = "Quick Quote";
     private String formatting = "Yes";
