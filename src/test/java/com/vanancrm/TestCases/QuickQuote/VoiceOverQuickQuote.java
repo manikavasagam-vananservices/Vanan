1a1206ada530457be53b21cd9831d8f50db76be9
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-12-10 16:20:14 +0530

    click view

--- src/test/java/com/vanancrm/TestCases/QuickQuote/VoiceOverQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/VoiceOverQuickQuote.java
@@ -9,6 +9,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -147,6 +148,8 @@ public class VoiceOverQuickQuote extends TestBase {
                 viewTicketDetails = readTableData.clickService(service,
                         (i + 1));
                 waitForProcessCompletion(20);
+                driver.findElement(By.id("view_btn")).click();
+                waitForProcessCompletion(10);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
                 if (viewTicketDetails.getEmailId()
