df72cdd54aa9a20d8ab4487f1c832f9fa19b2db8
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-12-10 15:20:16 +0530

    Click view comment added

--- src/test/java/com/vanancrm/TestCases/SentQuote/TranslationSentQuote.java
+++ src/test/java/com/vanancrm/TestCases/SentQuote/TranslationSentQuote.java
@@ -9,7 +9,7 @@ import java.util.List;
 import java.util.Properties;
 
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -176,6 +176,8 @@ public class TranslationSentQuote extends TestBase {
                 viewTicketDetails = readTableData.clickService(service,
                         (i + 1));
                 waitForProcessCompletion(20);
+                driver.findElement(By.id("view_btn")).click();
+                waitForProcessCompletion(10);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
                 if (viewTicketDetails.getEmailId()
