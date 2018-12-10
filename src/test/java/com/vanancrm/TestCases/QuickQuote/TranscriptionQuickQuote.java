dc3321c0634a0f5e4ee7dcfa26a1c929a0dc4143
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-12-10 16:12:55 +0530

    Update TranscriptionQuickQuote.java

--- src/test/java/com/vanancrm/TestCases/QuickQuote/TranscriptionQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/TranscriptionQuickQuote.java
@@ -7,8 +7,9 @@ import java.util.List;
 import java.util.Properties;
 
 import java.util.concurrent.TimeUnit;
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
+
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -147,6 +148,8 @@ public class TranscriptionQuickQuote extends TestBase {
                 viewTicketDetails = readTableData.clickService(service,
                         (i + 1));
                 waitForProcessCompletion(20);
+                driver.findElement(By.id("view_btn")).click();
+                waitForProcessCompletion(10);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
                 if (viewTicketDetails.getEmailId()
