99f213acead6f274dec08be63f5bc20f53fda1c5
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-12-10 16:51:44 +0530

    click view code

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TypingQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TypingQuote.java
@@ -11,6 +11,7 @@ import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Cookie;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -429,8 +430,10 @@ public class TypingQuote extends TestBase implements TypingPrice {
                 waitForProcessCompletion(20);
                 viewTicketDetails = new ViewTicketDetails(driver);
                 viewTicketDetails = readTableData.clickService(service,
-                        (i + 1));
+                        (i + 1));   
                 waitForProcessCompletion(20);
+                driver.findElement(By.id("view_btn")).click();
+                waitForProcessCompletion(10);
                 System.out.println("Channel " + viewTicketDetails
                         .getRunTimeTicketFieldValues("Channel"));
                 if (viewTicketDetails.getEmailId()
