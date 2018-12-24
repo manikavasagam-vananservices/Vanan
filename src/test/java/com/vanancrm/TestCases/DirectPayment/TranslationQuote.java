97452cca1ddfdbf1398c8bd4919d8c02f304f81a
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2018-12-24 09:36:00 +0530

    421 added

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TranslationQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TranslationQuote.java
@@ -418,6 +418,8 @@ public class TranslationQuote extends TestBase implements TranslationPrice {
                 viewTicketDetails = readTableData.clickService(service,
                         (i + 1));
                 waitForProcessCompletion(20);
+                 driver.findElement(By.id("view_btn")).click();
+                waitForProcessCompletion(10);
 
                 if (viewTicketDetails.getEmailId()
                         .contains(mailId) && viewTicketDetails
