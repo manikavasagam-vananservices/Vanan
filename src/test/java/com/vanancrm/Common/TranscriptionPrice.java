715882c76c65d4014196398447206ee14ab2f964
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-01-24 13:01:09 +0530

    Transcription price changed

--- src/test/java/com/vanancrm/Common/TranscriptionPrice.java
+++ src/test/java/com/vanancrm/Common/TranscriptionPrice.java
@@ -2,8 +2,8 @@ package com.vanancrm.Common;
 
 public interface TranscriptionPrice {
 
-    public double[] basePri = {0.99, 1.75, 2.00, 3.99};
-    public double[] discountPri = {0.89, 0.79, 0.69};
+    public double[] basePri = {1.00, 1.75, 2.00, 3.99};
+    public double[] discountPri = {0.90, 0.80, 0.70};
     public double[] timecodePri = {0.25, 0.50};
     public double additionalQtyPri = 0.25;
     public double transcationPri = 0.05;
