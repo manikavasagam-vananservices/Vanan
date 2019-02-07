c0df5dccf8718f51b2664004ef429c0624032650
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-02-07 18:10:30 +0530

    Cost changed

--- src/test/java/com/vanancrm/Common/CaptioningPrice.java
+++ src/test/java/com/vanancrm/Common/CaptioningPrice.java
@@ -2,8 +2,8 @@ package com.vanancrm.Common;
 
 public interface CaptioningPrice {
 
-    public double[] basePrices = {1.00, 3.99, 7.00};
-    public double[] transcriptionPrices = {0.99, 3.49, 5.00};
+    public double[] basePrices = {2.00, 9.98, 7.00};
+    public double[] transcriptionPrices = {1.00, 3.49, 5.00};
     public double[] translationPrices = {4.99, 7.00};
     public double transcationPri = 0.05;
     public double[] timeCodePri = {1.00};
