d78df7b8debfdce1544ff1c799c5e018243f4e1c
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-04-22 12:45:02 +0530

    220 commented

--- src/test/java/com/vanancrm/TestCases/DirectPayment/WritingService.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/WritingService.java
@@ -217,7 +217,7 @@ public class WritingService extends TestBase {
         Edit edit = menus.clickEdit();
         edit.selectPaymentType("Full payment");
         edit.selectPaymentMode("Square");
-        edit.selectStatus("Others");
+      //  edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
         emailConversation = menus.clickEmailConversation();
