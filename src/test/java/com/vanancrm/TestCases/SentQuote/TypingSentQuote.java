70e9e15a8a86aa2fd61a6bc4a9f0c6940d7a2589
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-04-22 14:55:27 +0530

    Update TypingSentQuote.java

--- src/test/java/com/vanancrm/TestCases/SentQuote/TypingSentQuote.java
+++ src/test/java/com/vanancrm/TestCases/SentQuote/TypingSentQuote.java
@@ -231,7 +231,7 @@ public class TypingSentQuote extends TestBase {
         Edit edit = menus.clickEdit();
         edit.selectPaymentType("Full payment");
         edit.selectPaymentMode("Square");
-        edit.selectStatus("Others");
+    //    edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
         emailConversation = menus.clickEmailConversation();
