94adb91830328a50f5b1156f352950d2c9b707d9
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-04-22 14:58:53 +0530

    Update TranscriptionQuickQuote.java

--- src/test/java/com/vanancrm/TestCases/QuickQuote/TranscriptionQuickQuote.java
+++ src/test/java/com/vanancrm/TestCases/QuickQuote/TranscriptionQuickQuote.java
@@ -211,7 +211,7 @@ public class TranscriptionQuickQuote extends TestBase {
         Edit edit = menus.clickEdit();
         edit.selectPaymentType("Full payment");
         edit.selectPaymentMode("Square");
-        edit.selectStatus("Others");
+  //      edit.selectStatus("Others");
         edit.clickUpdateButton();
         waitForProcessCompletion(10);
         emailConversation = menus.clickEmailConversation();
