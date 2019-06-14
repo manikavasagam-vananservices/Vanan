b64804c81f184668e342111679df9877dbe0d9df
manikavasagam-vananservices <32132366+manikavasagam-vananservices@users.noreply.github.com>
2019-06-14 17:51:46 +0530

    Update TranslationQuote.java

--- src/test/java/com/vanancrm/TestCases/DirectPayment/TranslationQuote.java
+++ src/test/java/com/vanancrm/TestCases/DirectPayment/TranslationQuote.java
@@ -443,7 +443,7 @@ public class TranslationQuote extends TestBase implements TranslationPrice {
                  //waitForProcessCompletion(10);
                 //System.out.println("View"+driver.findElement(By.id("view_btn")).getText());
                 JavascriptExecutor jse = (JavascriptExecutor)drive;
-                jse.executeScript("scroll(0, -100);")
+                jse.executeScript("scroll(0, -100);");
                driver.findElement(By.id("view_btn")).click();
               //  VIEWBUTTON = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("view_btn")));
               //  VIEWBUTTON.click();
